590e9fe8d4adfe41e26b6caacc4d2a65ee912822
Joel <JoelFuelling@gmail.com>
2023-05-08 20:33:27 -0500

    morning portion completed for DjangoApp homework. Home page renders with 'base.html' view now.

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+die = [ 
+  {'sides': '20', 'material': 'Polyurethane', 'color': 'red', 'text_color': 'Black'},
+  {'sides': '12', 'material': 'Brass', 'color': 'Brass', 'text_color': 'Brass'},
+  {'sides': '10', 'material': 'Polyurethane', 'color': 'black', 'text_color': 'White'},
+  {'sides': '8', 'material': 'Wood', 'color': 'Woodgrain', 'text_color': 'Burnt texture'},
+  {'sides': '6', 'material': 'Polyurethane', 'color': 'red', 'text_color': 'Black'},
+  {'sides': '4', 'material': 'Polyurethane', 'color': 'red', 'text_color': 'Black'},
+]
 # Define the home view
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def die_index(request):
+  return render(request, 'die/index.html', {
+    'die': die
+  })
+                
